0346cb06a917d0523c61e974dcaa4e7db676452f
dpgaspar <danielvazgaspar@gmail.com>
2014-01-12 15:40:24 +0000

    import correction

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def desc():
 
 setup(
     name='Flask-BabelPkg',
-    version='0.9.1',
+    version='0.9.2',
     url='http://github.com/dpgaspar/flask-babelPkg',
     license='BSD',
     author='Daniel Gaspar',

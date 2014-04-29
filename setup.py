708735ec259652009357455919a933c63972f9d2
dpgaspar <danielvazgaspar@gmail.com>
2014-04-29 10:47:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def desc():
 
 setup(
     name='Flask-BabelPkg',
-    version='0.9.5',
+    version='0.9.7',
     url='http://github.com/dpgaspar/flask-babelPkg',
     license='BSD',
     author='Daniel Gaspar',
